bc166a752e7b1026cb110225b1f4870148fcb16e
Fabien Thouraud <fabien.thouraud@outlook.com>
2021-12-01 22:53:40 +0100

    feat: day 1

---
+++ app/src/main/kotlin/me/fth/aoc/day1/Day1.kt
@@ -0,0 +1,11 @@
+package me.fth.aoc.day1
+
+import me.fth.aoc.readInputLines
+
+fun resolvePartOne(): Int = readInputLines(1) { lines ->
+    lines.zipWithNext { a, b -> if (a.toInt() < b.toInt()) 1 else 0 }.sum()
+}
+
+fun resolvePartTwo(): Int = readInputLines(1) { lines ->
+    lines.windowed(3, 1, true).map { window -> window.sumOf { it.toInt() } }.zipWithNext { a, b -> if (a < b) 1 else 0 }.sum()
+}

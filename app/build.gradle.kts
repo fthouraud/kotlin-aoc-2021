4ee5c26471a2a4676c734bc749471bb28da18613
Fabien Thouraud <fabien.thouraud@ekino.com>
2021-12-06 09:47:44 +0100

    chore: add a task to init a new day files and directories

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.time.LocalDate
+
 plugins {
     kotlin("jvm") version "1.6.0"
 }
@@ -18,5 +20,22 @@ testing {
         val test by getting(JvmTestSuite::class) {
             useKotlinTest()
         }
+    }
+}
+
+tasks.create("initDay").doFirst {
+    val dayOfMonth = LocalDate.now().dayOfMonth
+    val projectDirectory = layout.projectDirectory.asFile
+
+    with(File(projectDirectory, """src/main/kotlin/me/fth/aoc/day$dayOfMonth""")) {
+        mkdir()
+        File(this, """Day$dayOfMonth.kt""").createNewFile()
+    }
+
+    File(projectDirectory, """src/main/resources/day$dayOfMonth.txt""").createNewFile()
+
+    with(File(projectDirectory, """src/test/kotlin/me/fth/aoc/day$dayOfMonth""")) {
+        mkdir()
+        File(this, """Day${dayOfMonth}Test.kt""").createNewFile()
     }
 }

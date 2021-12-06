bce6681cf0b95a49d238306b8bb61f7569a85a3b
Fabien Thouraud <fabien.thouraud@outlook.com>
2021-12-06 08:59:55 +0100

    feat: day 5 (partial)

---
+++ app/src/main/kotlin/me/fth/aoc/day5/Day5.kt
@@ -0,0 +1,83 @@
+package me.fth.aoc.day5
+
+import me.fth.aoc.readInputLines
+import kotlin.math.absoluteValue
+import kotlin.math.max
+import kotlin.math.min
+
+data class Point(val x: Int, val y: Int)
+data class Line(val a: Point, val b: Point)
+
+const val diagramSize = 999
+
+fun resolvePartOne() = readInputLines(5) {
+    val diagram = initDiagram()
+    parseLines(it).filter { line -> line.isHorizontal() || line.isVertical() }.forEach { line ->
+        line.getPoints().forEach { p -> diagram[p.y][p.x]++ }
+    }
+    diagram.forEach { row -> println(row.joinToString(" ")) }
+    diagram.sumOf { row -> row.count { v -> v >= 2 } }
+}
+
+fun initDiagram() = Array(diagramSize) { IntArray(diagramSize) { 0 } }
+
+fun parseLines(input: Sequence<String>): Sequence<Line> {
+    val pointRegex = """(\d+),(\d+)""".toRegex()
+    return input.flatMap { it.split(" -> ") }
+        .map { pointRegex.matchEntire(it)?.destructured?.let { (x, y) -> Point(x.toInt(), y.toInt()) } }
+        .filterNotNull()
+        .windowed(2, 2) { (a, b) -> Line(a, b) }
+}
+
+fun Line.getPoints() = when {
+    isDiagonal() -> when {
+        a.x == a.y && b.x == b.y -> getRange(a.x, b.x).map { Point(it, it) }
+        a.x == b.y && a.y == b.x -> {
+            val diff = (a.x - a.y).absoluteValue
+            getRange(0, diff).map { Point(min(a.x, a.y) + it, max(b.x, b.y) - it) }
+        }
+        a.x == a.y || b.x == b.y -> {
+            if (a.x == a.y) {
+                if (b.x > b.y) {
+                    val diff = (b.x - a.x).absoluteValue
+                    getRange(0, diff).map { Point(a.x + it, a.x - it) }
+                } else {
+                    val diff = (b.y - a.y).absoluteValue
+                    getRange(0, diff).map { Point(a.x - it, a.x + it) }
+                }
+            } else {
+                if (a.x > a.y) {
+                    val diff = (a.x - b.x).absoluteValue
+                    getRange(0, diff).map { Point(b.x + it, b.x - it) }
+                } else {
+                    val diff = (a.y - b.y).absoluteValue
+                    getRange(0, diff).map { Point(b.x - it, b.x + it) }
+                }
+            }
+        }
+        else -> {
+            val xRange = getRange(a.x, b.x).iterator()
+            getRange(a.y, b.y).map { y -> Point(xRange.next(), y) }
+        }
+    }
+    isHorizontal() -> getRange(a.y, b.y).map { Point(a.x, it) }
+    isVertical() -> getRange(a.x, b.x).map { Point(it, a.y) }
+    else -> emptyList()
+}
+
+fun Line.isHorizontal() = a.x == b.x
+
+fun Line.isVertical() = a.y == b.y
+
+fun getRange(a: Int, b: Int) = if (a > b) b..a else a..b
+
+fun resolvePartTwo() = readInputLines(5) {
+    val diagram = initDiagram()
+    parseLines(it).forEach { line ->
+        line.getPoints().forEach { p -> diagram[p.y][p.x]++ }
+    }
+    diagram.forEach { row -> println(row.joinToString(" ")) }
+    diagram.sumOf { row -> row.count { v -> v >= 2 } }
+}
+
+fun Line.isDiagonal() = !(isHorizontal() || isVertical())
---
+++ app/src/test/kotlin/me/fth/aoc/day5/Day5Test.kt
@@ -0,0 +1,55 @@
+package me.fth.aoc.day5
+
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+class Day5Test {
+    @Test
+    fun `resolvePartOne should return 8060 as power consumption`() {
+        assertEquals(8060, resolvePartOne())
+    }
+
+    @Test
+    fun `resolvePartTwo should return 21810 as power consumption`() {
+        assertEquals(21810, resolvePartTwo())
+    }
+    @Test
+    fun `getPoints should return 3 points`() {
+        val linePoints = Line(Point(1, 1), Point(3, 3)).getPoints()
+
+        assertEquals(3, linePoints.size)
+        assertContains(linePoints, Point(1, 1))
+        assertContains(linePoints, Point(2, 2))
+        assertContains(linePoints, Point(3, 3))
+    }
+    @Test
+    fun `getPoints should return 3 points (2)`() {
+        val linePoints = Line(Point(9, 7), Point(7, 9)).getPoints()
+
+        assertEquals(3, linePoints.size)
+        assertContains(linePoints, Point(9, 7))
+        assertContains(linePoints, Point(8, 8))
+        assertContains(linePoints, Point(7, 9))
+    }
+    @Test
+    fun `getPoints should return 5 points`() {
+        val linePoints = Line(Point(6, 4), Point(2, 0)).getPoints()
+
+        assertEquals(5, linePoints.size)
+        assertContains(linePoints, Point(6, 4))
+        assertContains(linePoints, Point(5, 3))
+        assertContains(linePoints, Point(4, 2))
+        assertContains(linePoints, Point(3, 1))
+        assertContains(linePoints, Point(2, 0))
+    }
+    @Test
+    fun `getPoints should return 3 points (4)`() {
+        val linePoints = Line(Point(5, 5), Point(8, 2)).getPoints()
+
+//        assertEquals(4, linePoints.size)
+        assertContains(linePoints, Point(9, 7))
+        assertContains(linePoints, Point(8, 8))
+        assertContains(linePoints, Point(7, 9))
+    }
+}
